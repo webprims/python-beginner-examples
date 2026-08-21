4e43fa9380cce95f34a68163055566ce20c2a5a0
WebPrims <webprims@gmail.com>
2026-08-21 15:30:03 +0530

    Add list example

---
+++ 05-collections/lists.py
@@ -0,0 +1,8 @@
+technologies = ["Python", "React", "AI"]
+
+technologies.append("IoT")
+print("All technologies:", technologies)
+print("First item:", technologies[0])
+
+for technology in technologies:
+    print(technology)
