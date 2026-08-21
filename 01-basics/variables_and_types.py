960caefbca5e396396ea9c95723af2a42d831fd9
WebPrims <webprims@gmail.com>
2026-08-21 15:29:04 +0530

    Add variables and data types example

---
+++ 01-basics/variables_and_types.py
@@ -0,0 +1,14 @@
+name = "Aman"
+age = 20
+height = 5.9
+is_learning_python = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Learning Python:", is_learning_python)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_learning_python))
